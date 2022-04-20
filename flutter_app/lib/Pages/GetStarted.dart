a772b41c4d15f72615665df2c2903f07b2e07862
MichaelC005 <michaelchris0501@gmail.com>
2022-04-20 19:54:25 +0700

    RecommendPage

--- flutter_app/lib/Pages/GetStarted.dart
+++ flutter_app/lib/Pages/GetStarted.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_skripsi/Pages/BuildSchemaPage.dart';
+import 'package:project_skripsi/Pages/RecommendedFormPage.dart';
 import 'package:project_skripsi/UI/CorneredButton.dart';
 import 'package:project_skripsi/UI/Palette.dart';
 import 'package:project_skripsi/UI/TitledContainer.dart';
@@ -13,92 +14,109 @@ class GetStartedPage extends StatefulWidget {
 }
 
 class _GetStartedPageState extends State<GetStartedPage> {
-
   @override
   Widget build(BuildContext context) {
-    return
-      Scaffold(
-          body: Stack(
-            children: [
-              SafeArea(
-                child: CustomAppbar(
-                  title: "",
-                  sideBarOpacity: 0,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+    return Scaffold(
+        body: Stack(
+      children: [
+        SafeArea(
+          child: CustomAppbar(
+            title: "",
+            sideBarOpacity: 0,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TitledContainer(
+                  withBottomRightBorder: true,
+                  title: "Get Started",
+                  height: MediaQuery.of(context).size.height * 0.35,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      TitledContainer(
-                        withBottomRightBorder: true,
-                        title: "Get Started",
-                        height: MediaQuery.of(context).size.height * 0.35,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                    child: CorneredButton(
-                                      onPressed: (){
-                                        Navigator.push(
-                                          context,
-                                          PageRouteBuilder(
-                                            pageBuilder: (context, animation1, animation2) => const BuildSchemaPage(),
-                                            transitionDuration: Duration.zero,
-                                          ),
-                                        );
-                                      },
-                                      child: Container(
-                                        padding: const EdgeInsets.only(top: 15, bottom: 15),
-                                        child: Text("Start a new build", style: TextStyles.interStyle1,),
-                                      ),
-                                    )
-                                )
-                              ],
+                      Row(
+                        children: [
+                          Expanded(
+                              child: CorneredButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                  pageBuilder:
+                                      (context, animation1, animation2) =>
+                                          const BuildSchemaPage(),
+                                  transitionDuration: Duration.zero,
+                                ),
+                              );
+                            },
+                            child: Container(
+                              padding:
+                                  const EdgeInsets.only(top: 15, bottom: 15),
+                              child: Text(
+                                "Start a new build",
+                                style: TextStyles.interStyle1,
+                              ),
                             ),
-                            Row(
-                              children: [
-                                Expanded(
-                                    child: CorneredButton(
-                                      onPressed: (){},
-                                      child:  Container(
-                                        padding: const EdgeInsets.only(top: 15, bottom: 15),
-                                        child: Text("Get recommended build", style: TextStyles.interStyle1,),
-                                      ),
-                                    )
-                                )
-                              ],
+                          ))
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                              child: CorneredButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                  pageBuilder:
+                                      (context, animation1, animation2) =>
+                                          const RecommendedFormPage(),
+                                  transitionDuration: Duration.zero,
+                                ),
+                              );
+                            },
+                            child: Container(
+                              padding:
+                                  const EdgeInsets.only(top: 15, bottom: 15),
+                              child: Text(
+                                "Get recommended build",
+                                style: TextStyles.interStyle1,
+                              ),
                             ),
-                            Row(
-                              children: [
-                                Expanded(
-                                    child: CorneredButton(
-                                      onPressed: (){},
-                                      child: Container(
-                                        padding: const EdgeInsets.only(top: 15, bottom: 15),
-                                        child: Text("Import Build", style: TextStyles.interStyle1,),
-                                      ),
-                                    )
-                                )
-                              ],
-                            )
-                          ],
-                        ),
+                          ))
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                              child: CorneredButton(
+                            onPressed: () {},
+                            child: Container(
+                              padding:
+                                  const EdgeInsets.only(top: 15, bottom: 15),
+                              child: Text(
+                                "Import Build",
+                                style: TextStyles.interStyle1,
+                              ),
+                            ),
+                          ))
+                        ],
                       )
                     ],
-                  )
-                ],
-              ),
-            ],
-          ) // This trailing comma makes auto-formatting nicer for build methods.
-      );
-
-
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ],
+    ) // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
