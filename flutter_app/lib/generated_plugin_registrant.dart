97eb21c30f7b140bd6b9646f035f62b2e84c578a
Andi Prasetyo <tioe.andi.dev@gmail.com>
2022-06-15 18:29:41 +0700

    Code cleanup

--- flutter_app/lib/generated_plugin_registrant.dart
+++ flutter_app/lib/generated_plugin_registrant.dart
@@ -6,8 +6,9 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:connectivity_plus_web/connectivity_plus_web.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
